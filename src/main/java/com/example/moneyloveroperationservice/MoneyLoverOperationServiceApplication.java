9846505bd9d59ad9d398543ec84a1c9418b0ad29
dankos <daniil.koryto@gmail.com>
2021-09-23 06:10:34 +1000

    Initial commit

---
+++ src/main/java/com/example/moneyloveroperationservice/MoneyLoverOperationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.moneyloveroperationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyLoverOperationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyLoverOperationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moneyloveroperationservice/MoneyLoverOperationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneyloveroperationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyLoverOperationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
